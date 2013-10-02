8918cc3cc6c7ebaf7fb1995ad502c79a2961fc60
ThallesScheffer <thalles.a.s.c@gmail.com>
2013-10-02 19:40:26 -0300

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -1,3 +1,24 @@
-var textareaX = document.getElementById( 'seu_textarea' ).innerHTML;
-textareaX = textareaX.replace(/Fans/gi, "loucos!");
-document.getElementById( 'seu_textarea' ).innerHTML = textareaX;
+}
+
+function onCookiesLoaded() {
+	if (autowoot) {
+		setTimeout("$('#button-vote-positive').click();", 7000);
+	}
+	if (autoqueue && !isInQueue()) {
+		joinQueue();
+	}
+	if (hideVideo) {
+		$('#yt-frame').animate({'height': (hideVideo ? '0px' : '271px')}, {duration: 'fast'});
+		$('#playback .frame-background').animate({'opacity': (hideVideo ? '0' : '0.91')}, {duration: 'medium'});
+	}
+	if (left) {
+		$(".sidebar#side-left").animate({"left": left ? "0px" : "-190px"}, 300, "easeOutCirc");
+	}
+	if (!audience) {
+		$('#audience').hide();
+	}
+    	initAPIListeners();
+    	displayUI();
+    	initUIListeners();
+    	populateUserlist();
+}
