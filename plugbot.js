de29c0316f1b9da9ee96a5cf2cf6525816923a22
ThallesScheffer <thalles.a.s.c@gmail.com>
2013-10-04 17:35:05 -0300

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -1,22 +1,34 @@
-function animacao(obj) {
-	var YY = -20;
-	var XX = 1;
-	var el = document.getElementById(obj);
-	if(el.style.display != "block") {
-		var teste = window.setInterval(function() {
-		YY++;
-		if (YY < 1) {
-			el.style.display = "block";
-			el.style.position = "relative";
-			el.style.top=YY+document.body.scrollTop;
-		}
-	},20);
-	}else {
-	var teste = window.setInterval(function() {
-		XX=XX-1;
-		if (XX > -20) {
-			el.style.top=XX+document.body.scrollTop;
-		}else { clearInterval(teste);el.style.display = "none"; }
-	},20);
-	}
+var minimo=1;
+var maximo=240;
+var co=-250;
+var vel1=1;
+var abri=0;
+var menus_do_ianez = document.createElement("div");
+menus_do_ianez.setAttribute("id", "divs");
+menus_do_ianez.setAttribute("class", "");
+document.body.appendChild(menus_do_ianez);
+document.getElementById("divs").className="menutodos";
+function criar_menu(){//Este script cria o menu feito dentro do textarea.
+menu.value=menu.value.replace(/§0 /g , "<div class='menos'>");
+menu.value=menu.value.replace(/§1 /g , "<div class='off' onmouseover=this.className='on' onmouseout=this.className='off' onclick=document.location.href='");
+menu.value=menu.value.replace(/ §2 /g , "'>");
+menu.value=menu.value.replace(/ §3/g , "</div>");
+menu.value=menu.value.replace(/§5 /g , "<div class='off' onmouseover=this.className='on' onmouseout=this.className='off' onclick='");
+divs.innerHTML=""+menu.value+"";
 }
+function mover(){//Este script diz se o mouse esta perto para abrir ou longe para fechar.
+var rato=event.clientX;
+if(rato<=minimo){abri=1;vel1=1;movimento();}
+if(rato>=maximo){abri=0;vel1=1;movimento();}
+}
+function movimento(){//Este script faz o menu abrir e fechar.
+if(co>=minimo){co=minimo;}
+else
+if(abri==1){co+=1;}
+if(abri==0){co-=1;}
+if(co>=minimo){vel1=9999999999999;co=minimo;}
+if(co<=-250){vel1=99999999999;co=-250;}
+divs.style.left=co;
+setTimeout("movimento()",vel1);
+}
+criar_menu();
